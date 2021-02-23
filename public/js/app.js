6718eb9a9c8433e4e7ef41cb920e63a186c05034
samgozman <sam@gozman.space>
2021-02-23 18:33:23 +0300

    Добавлены модальные окна и прочие улучшения

--- public/js/app.js
+++ public/js/app.js
@@ -254,7 +254,8 @@ const erase = (word = ' пусто ') => {
 // Is loading - add loading styles
 const isLoading = (bool = true) => {
     const targetIDs = [
-        'submit_button'
+        'submit_button',
+        'input_ticker_control'
     ]
 
     targetIDs.forEach(id => {
@@ -403,3 +404,57 @@ form.addEventListener('submit', async (e) => {
         error_message.textContent = error.message == 429 ? 'Превышен лимит запросов в минуту!' : 'Ошибка! Введите правильный тикер'
     }
 })
+
+//! SET MODAL EVENT LISTNERS
+//? Source: https://siongui.github.io/2018/02/11/bulma-modal-with-javascript/
+document.addEventListener('DOMContentLoaded', function () {
+
+  // Modals
+
+  const rootEl = document.documentElement
+  const modals = getAll('.modal')
+  const modalButtons = getAll('.modal-button')
+  const modalCloses = getAll('.modal-background, .modal-close, .modal-card-head .delete, .modal-card-foot .button');
+
+  if (modalButtons.length > 0) {
+    modalButtons.forEach(function (el) {
+      el.addEventListener('click', function () {
+        const target = el.dataset.target;
+        const $target = document.getElementById(target);
+        rootEl.classList.add('is-clipped')
+        $target.classList.add('is-active')
+      })
+    })
+  }
+
+  if (modalCloses.length > 0) {
+    modalCloses.forEach(function (el) {
+      el.addEventListener('click', function () {
+        closeModals();
+      })
+    })
+  }
+
+  document.addEventListener('keydown', function (event) {
+    const e = event || window.event;
+    if (e.keyCode === 27) {
+      closeModals()
+    }
+  })
+
+  function closeModals() {
+    rootEl.classList.remove('is-clipped')
+    modals.forEach(function (el) {
+      el.classList.remove('is-active')
+    })
+  }
+
+  // Functions
+
+  function getAll(selector) {
+    return Array.prototype.slice.call(document.querySelectorAll(selector), 0)
+  }
+
+})
+
+//! END MODAL EVENT LISTNERS
