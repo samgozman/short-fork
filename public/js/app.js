ba2c9034b2e81187ce464f108fa06f13087e8651
samgozman <sam@gozman.space>
2021-02-24 20:54:46 +0300

    Добавил preloader

--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,23 @@
+
+// Preloader
+var preloader = document.getElementById("preloader_preload");
+
+function fadeOut(el) {
+    el.style.opacity = 1
+    const preloaderinterval = setInterval(function () {
+        el.style.opacity = el.style.opacity - 0.05
+        if (el.style.opacity <= 0.05) {
+            clearInterval(preloaderinterval)
+            preloader.style.display = "none"
+        }
+    }, 16)
+}
+window.onload = function () {
+    setTimeout(function () {
+        fadeOut(preloader)
+    }, 1000)
+}
+
 // Define chart and render it
 const chartVolume = new ApexCharts(document.querySelector("#chartVolume"), {
     series: [{
