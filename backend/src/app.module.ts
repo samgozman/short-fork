4892193845b53d1671aa766bb2750a97c617f604
samgozman <sam@gozman.space>
2023-01-10 23:32:38 +0400

    Config Redis as a cache manager

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,50 @@
-import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { redisStore } from 'cache-manager-redis-store';
+import { CacheModule, Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import Joi from 'joi';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: `config/${process.env.NODE_ENV}.env`,
+      validationSchema: Joi.object({
+        NODE_ENV: Joi.string()
+          .valid('development', 'production', 'test')
+          .default('development'),
+        PORT: Joi.number().default(3000),
+        REDIS_HOST: Joi.string().default('localhost'),
+        REDIS_PORT: Joi.number().default(6379),
+        REDIS_PASSWORD: Joi.string().default(''),
+      }),
+    }),
+    CacheModule.registerAsync<any>({
+      isGlobal: true,
+      imports: [ConfigModule],
+      useFactory: async (config: ConfigService) => {
+        console.log(
+          process.env.NODE_ENV,
+          config.get('REDIS_PORT'),
+          config.get('REDIS_PASSWORD'),
+        );
+        const store = await redisStore({
+          socket: {
+            host: config.get('REDIS_HOST'),
+            port: config.get('REDIS_PORT'),
+            passphrase: config.get('REDIS_PASSWORD'),
+          },
+          database: 0,
+          ttl: 60,
+        });
+        return {
+          store: () => store,
+        };
+      },
+      inject: [ConfigService],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
