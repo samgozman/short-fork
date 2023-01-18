90d5f7ba488dd9b43234f13ec5605c5379d58134
samgozman <sam@gozman.space>
2023-01-18 10:11:53 +0400

    Throw 404 if not found

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -1,4 +1,10 @@
-import { Controller, Get, Param, UseFilters } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  NotFoundException,
+  Param,
+  UseFilters,
+} from '@nestjs/common';
 import { AppService } from './app.service';
 import { HttpExceptionFilter } from './exceptions/http-exception.filter';
 
@@ -6,9 +12,14 @@ import { HttpExceptionFilter } from './exceptions/http-exception.filter';
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+  // TODO: Cache this endpoint to prevent unnecessary requests
   @Get('finviz/:ticker')
   @UseFilters(new HttpExceptionFilter())
-  async getFinviz(@Param('ticker') ticker: string): Promise<any> {
-    return this.appService.getFinviz(ticker);
+  async getFinviz(@Param('ticker') ticker: string) {
+    const data = await this.appService.getFinviz(ticker);
+    if (!data) {
+      throw new NotFoundException();
+    }
+    return data;
   }
 }
