2be049349f1b4cc46e8f1488ac019045aea4f093
samgozman <sam@gozman.space>
2023-01-21 15:50:11 +0400

    Add tailwind to the project

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,6 +5,10 @@ import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    target: "es2020",
+    sourcemap: true, // Source map generation must be turned on for Sentry
+  },
   plugins: [vue()],
   resolve: {
     alias: {
