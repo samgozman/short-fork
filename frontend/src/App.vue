5e1cacec0434968c2882ac50df12eb759f6faaf1
samgozman <sam@gozman.space>
2023-01-21 15:07:20 +0400

    Add frontend basic app

---
+++ frontend/src/App.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from "vue-router";
+</script>
+
+<template>
+  <header>
+    <div>
+      <HelloWorld msg="You did it!" />
+
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav>
+    </div>
+  </header>
+
+  <!-- <RouterView /> -->
+</template>
---
+++ frontend/src/views/HomeView.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <main>
+    <p>Hello World</p>
+  </main>
+</template>
