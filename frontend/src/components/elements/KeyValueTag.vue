bf9b8dc10bbc14bde7a2654bb4fac0067fbe0a10
samgozman <sam@gozman.space>
2023-01-26 10:13:50 +0400

    Add KeyValueTag element

---
+++ frontend/src/components/elements/KeyValueTag.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+enum TagThemes {
+  Gray = "gray",
+  Red = "red",
+  Green = "green",
+  Yellow = "yellow",
+}
+
+defineProps<{
+  /** Values: 'gray', 'red', 'green', 'yellow' */
+  theme: string;
+}>();
+</script>
+
+<template>
+  <div class="w-full flex h-6">
+    <span
+      class="bg-gray-700 text-gray-100 dark:bg-gray-700 dark:text-gray-100 text-xs font-medium rounded-l w-full leading-6 pl-2"
+    >
+      <slot name="key"></slot>
+    </span>
+    <span
+      class="text-xs font-medium rounded-r w-full leading-6 text-center"
+      :class="{
+        'bg-red-500 text-red-100 dark:bg-red-600 dark:text-red-100':
+          theme === TagThemes.Red,
+        'bg-green-500 text-green-100 dark:bg-green-600 dark:text-green-200':
+          theme === TagThemes.Green,
+        'bg-yellow-400 text-gray-800 dark:bg-yellow-500 dark:text-gray-700':
+          theme === TagThemes.Yellow,
+        'bg-gray-200 text-gray-700 dark:bg-gray-200 dark:text-gray-700':
+          theme === TagThemes.Gray,
+      }"
+    >
+      <slot name="value"></slot>
+    </span>
+  </div>
+</template>
--- frontend/src/components/widgets/MainWidget.vue
+++ frontend/src/components/widgets/MainWidget.vue
@@ -1,9 +1,34 @@
 <script setup lang="ts">
-import InputWithSubmit from "../elements/InputWithSubmit.vue";
+import InputWithSubmit from "@/components/elements/InputWithSubmit.vue";
+import KeyValueTag from "@/components/elements/KeyValueTag.vue";
 </script>
 
 <template>
-  <InputWithSubmit @submitStock="submitStock" />
+  <div class="w-full">
+    <InputWithSubmit @submitStock="submitStock" />
+    <div class="w-full">
+      <p>Some general info</p>
+    </div>
+    <!-- Block with tags  -->
+    <div class="grid grid-cols-2 gap-2">
+      <KeyValueTag theme="gray">
+        <template #key>Key 1</template>
+        <template #value>Value 1</template>
+      </KeyValueTag>
+      <KeyValueTag theme="green">
+        <template #key>Key2</template>
+        <template #value>Value 2</template>
+      </KeyValueTag>
+      <KeyValueTag theme="red">
+        <template #key>Key2</template>
+        <template #value>Value 2</template>
+      </KeyValueTag>
+      <KeyValueTag theme="yellow">
+        <template #key>Key2</template>
+        <template #value>Value 2</template>
+      </KeyValueTag>
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
