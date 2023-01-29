26e67eddcbcd3d0cd7d251d3306e20b1e42b0a46
samgozman <sam@gozman.space>
2023-01-29 11:23:22 +0400

    round table

--- frontend/src/components/widgets/InsidersTableWidget.vue
+++ frontend/src/components/widgets/InsidersTableWidget.vue
@@ -17,7 +17,7 @@ defineProps<{
   <div class="w-full">
     <HeaderH5>Insiders trades</HeaderH5>
 
-    <div class="relative overflow-x-auto max-h-96">
+    <div class="relative overflow-x-auto max-h-96 rounded">
       <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
         <thead
           class="text-xs text-gray-700 uppercase border-b-4 dark:text-gray-400"
