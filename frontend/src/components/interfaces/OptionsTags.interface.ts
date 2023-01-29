fb8cf2da66465c12ea4ea486368e301fb1ace5ca
samgozman <sam@gozman.space>
2023-01-29 18:38:02 +0400

    Render options tags from array

---
+++ frontend/src/components/interfaces/OptionsTags.interface.ts
@@ -0,0 +1,12 @@
+export interface IOptionsTags {
+  impliedVolatility: number;
+  historicalVolatility: number;
+  ivPercentile: number;
+  ivRank: number;
+  putCallVolRatio: number;
+  todaysVolume: number;
+  volumeAvg30Day: number;
+  putCallOiRatio: number;
+  todaysOpenInterest: number;
+  openInt30Day: number;
+}
--- frontend/src/components/widgets/OptionsWidget.vue
+++ frontend/src/components/widgets/OptionsWidget.vue
@@ -18,47 +18,118 @@ defineProps<{
 
 <script lang="ts">
 import { defineComponent } from "vue";
-import type { TagElement } from "@/components/interfaces/TagElement.interface";
+import type { ITagElement } from "@/components/interfaces/TagElement.interface";
+import type { IOptionsTags } from "@/components/interfaces/OptionsTags.interface";
+import { TagThemes } from "@/components/enums/TagThemes.enum";
+import { getTagColor } from "@/components/utils/getTagColor";
+
+interface Data {
+  /** To store key:value data from API */
+  tagsValues: IOptionsTags;
+  /** To store tags for rendering */
+  tags: ITagElement[];
+}
 
 export default defineComponent({
-  data() {
+  data(): Data {
     return {
-      tags: [
-        {
-          key: "Key 5",
-          value: "Value 1",
-          theme: "gray",
-          title: "Some title for tag 1",
-          description: "some description for tag 1",
-        },
-        {
-          key: "Key 6",
-          value: "Value 2",
-          theme: "green",
-          title: "Some title for tag 2",
-          description: "some description for tag 2",
-        },
-        {
-          key: "Key 7",
-          value: "Value 3",
-          theme: "gray",
-          title: "Some title for tag 3",
-          description: "some description for tag 3",
-        },
-        {
-          key: "Key 8",
-          value: "Value 4",
-          theme: "gray",
-          title: "Some title for tag 4",
-          description: "some description for tag 4",
-        },
-      ] as TagElement[],
+      tagsValues: {} as IOptionsTags,
+      tags: [],
     };
   },
   methods: {
     submitStock(stock: string) {
       console.log(stock);
     },
+    prepareTags() {
+      this.tags = [
+        {
+          key: "IV",
+          value: (this.tagsValues.impliedVolatility ?? "") + "%",
+          theme: TagThemes.Gray,
+          title: "Some title for tag 4",
+          description: "some description for tag 4",
+        },
+        {
+          key: "Volat Avg",
+          value: (this.tagsValues.historicalVolatility ?? "") + "%",
+          theme: TagThemes.Gray,
+          title: "Some title for tag 4",
+          description: "some description for tag 4",
+        },
+        {
+          key: "Tod Vol",
+          value: this.tagsValues.todaysVolume ?? "",
+          theme: TagThemes.Gray,
+          title: "Some title for tag 4",
+          description: "some description for tag 4",
+        },
+        {
+          key: "Vol Avg",
+          value: this.tagsValues.volumeAvg30Day ?? "",
+          theme: TagThemes.Gray,
+          title: "Some title for tag 4",
+          description: "some description for tag 4",
+        },
+        {
+          key: "TOD OI",
+          value: this.tagsValues.todaysOpenInterest ?? "",
+          theme: TagThemes.Gray,
+          title: "Some title for tag 4",
+          description: "some description for tag 4",
+        },
+        {
+          key: "OI Avg",
+          value: this.tagsValues.openInt30Day ?? "",
+          theme: TagThemes.Gray,
+          title: "Some title for tag 4",
+          description: "some description for tag 4",
+        },
+        {
+          key: "PCR",
+          value: this.tagsValues.putCallVolRatio ?? "",
+          theme: getTagColor(this.tagsValues.putCallVolRatio, {
+            best: [0, 0.7],
+            worst: [1, Infinity],
+          }),
+          title: "Some title for tag 4",
+          description: "some description for tag 4",
+        },
+        {
+          key: "PCR (OI)",
+          value: this.tagsValues.putCallOiRatio ?? "",
+          theme: getTagColor(this.tagsValues.putCallOiRatio, {
+            best: [0, 0.7],
+            worst: [1, Infinity],
+          }),
+          title: "Some title for tag 4",
+          description: "some description for tag 4",
+        },
+        {
+          key: "IV Rank",
+          value: (this.tagsValues.ivRank ?? "") + "%",
+          theme: getTagColor(this.tagsValues.ivRank, {
+            best: [0, 30],
+            worst: [70, Infinity],
+          }),
+          title: "Some title for tag 4",
+          description: "some description for tag 4",
+        },
+        {
+          key: "IV PC",
+          value: (this.tagsValues.ivPercentile ?? "") + "%",
+          theme: getTagColor(this.tagsValues.ivPercentile, {
+            best: [0, 30],
+            worst: [70, Infinity],
+          }),
+          title: "Some title for tag 4",
+          description: "some description for tag 4",
+        },
+      ];
+    },
+  },
+  mounted() {
+    this.prepareTags();
   },
 });
 </script>
