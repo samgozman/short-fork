4696c4ae07bbe52a98977d231f7ab8dc51664e96
samgozman <sam@gozman.space>
2023-02-03 10:49:46 +0400

    Add apexcharts to the project

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,4 +1,6 @@
 import { createApp } from "vue";
+import VueApexCharts from "vue3-apexcharts";
+import ApexCharts from "apexcharts";
 import App from "./App.vue";
 import router from "./router";
 
@@ -7,5 +9,13 @@ import "./assets/main.css";
 const app = createApp(App);
 
 app.use(router);
+
+app.use(VueApexCharts);
+app.config.globalProperties.$apexcharts = ApexCharts;
+declare module "@vue/runtime-core" {
+  interface ComponentCustomProperties {
+    $apexcharts: typeof ApexCharts;
+  }
+}
 
 app.mount("#app");
