bccfd8e60b5c3466243c739800a76f4caf4f3bc0
samgozman <sam@gozman.space>
2023-02-13 09:37:21 +0400

    Fix navbar align

--- frontend/src/components/elements/navigation/NavBar.vue
+++ frontend/src/components/elements/navigation/NavBar.vue
@@ -3,19 +3,20 @@
     class="bg-white border-gray-200 px-2 sm:px-4 py-2 rounded-b dark:bg-gray-900 shadow-sm"
   >
     <div class="container flex flex-wrap items-center justify-between mx-auto">
-      <a href="#" class="flex items-center">
+      <a href="#" class="flex items-center md:w-[160px]">
         <span
-          class="self-center text-xl font-semibold whitespace-nowrap dark:text-white"
+          class="self-center text-xl font-semibold whitespace-nowrap dark:text-white px-4"
           >Short-fork</span
         >
       </a>
-      <div class="flex items-center md:order-2">
-        <label class="relative inline-flex items-center cursor-pointer">
+      <div
+        class="flex justify-between md:justify-end items-center md:order-2 md:w-[160px]"
+      >
+        <label class="relative inline-flex items-center cursor-pointer mr-1">
           <input type="checkbox" value="" class="sr-only peer" checked />
           <div
             class="w-11 h-6 bg-gray-200 rounded-full peer peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"
           ></div>
-
         </label>
 
         <button
