f27ddf1558e932a4c965f4c3f0ab9b7f8ff57063
samgozman <sam@gozman.space>
2023-02-13 10:07:49 +0400

    Add label for theme switcher

--- frontend/src/components/elements/navigation/NavBar.vue
+++ frontend/src/components/elements/navigation/NavBar.vue
@@ -12,37 +12,40 @@
       <div
         class="flex justify-between md:justify-end items-center md:order-2 md:w-[160px]"
       >
-        <label class="relative inline-flex items-center cursor-pointer mr-1">
-          <input type="checkbox" value="" class="sr-only peer" checked />
-          <div
-            :class="[
-              'w-11',
-              'h-6',
-              'bg-gray-200',
-              'rounded-full',
-              'peer',
-              'peer-focus:ring-4',
-              'peer-focus:ring-blue-300',
-              'dark:peer-focus:ring-blue-800',
-              'dark:bg-gray-700',
-              'peer-checked:after:translate-x-full',
-              'peer-checked:after:border-white',
-              'after:content-[\'\']',
-              'after:absolute',
-              'after:top-0.5',
-              'after:left-[2px]',
-              'after:bg-white',
-              'after:border-gray-300',
-              'after:border',
-              'after:rounded-full',
-              'after:h-5',
-              'after:w-5',
-              'after:transition-all',
-              'dark:border-gray-600',
-              'peer-checked:bg-blue-600',
-            ]"
-          ></div>
-        </label>
+        <div class="flex flex-col">
+          <label class="relative inline-flex items-center cursor-pointer mr-1">
+            <input type="checkbox" value="" class="sr-only peer" checked />
+            <div
+              :class="[
+                'w-11',
+                'h-6',
+                'bg-gray-200',
+                'rounded-full',
+                'peer',
+                'peer-focus:ring-4',
+                'peer-focus:ring-blue-300',
+                'dark:peer-focus:ring-blue-800',
+                'dark:bg-gray-700',
+                'peer-checked:after:translate-x-full',
+                'peer-checked:after:border-white',
+                'after:content-[\'\']',
+                'after:absolute',
+                'after:top-0.5',
+                'after:left-[2px]',
+                'after:bg-white',
+                'after:border-gray-300',
+                'after:border',
+                'after:rounded-full',
+                'after:h-5',
+                'after:w-5',
+                'after:transition-all',
+                'dark:border-gray-600',
+                'peer-checked:bg-blue-600',
+              ]"
+            ></div>
+          </label>
+          <span class="text-xs text-center leading-relaxed">Light</span>
+        </div>
 
         <button
           type="button"
