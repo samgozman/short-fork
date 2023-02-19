9c87a486641894b1fcc82a9b7b3a34b21a7c638f
samgozman <sam@gozman.space>
2023-02-19 10:18:10 +0400

    Add insiders table disclaimer

--- frontend/src/components/widgets/InsidersTableWidget.vue
+++ frontend/src/components/widgets/InsidersTableWidget.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import HeaderH5 from "@/components/layout/typography/HeaderH5.vue";
 import type { IInsider } from "@/components/interfaces/insider.interface";
+import HiddenParagraph from "@/components/layout/typography/HiddenParagraph.vue";
 
 defineProps<{
   insidersTrades: IInsider[];
@@ -8,51 +9,57 @@ defineProps<{
 </script>
 
 <template>
-  <div class="w-full">
+  <div class="flex flex-col h-full w-full">
     <HeaderH5>{{ $t("insidersTrades.title") }}</HeaderH5>
-
-    <div class="relative overflow-x-auto max-h-[29rem] rounded">
-      <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
-        <thead
-          class="text-xs text-gray-700 uppercase border-b-4 dark:text-gray-400"
+    <div class="grow">
+      <div class="relative overflow-x-auto max-h-[29rem] rounded">
+        <table
+          class="w-full text-sm text-left text-gray-500 dark:text-gray-400"
         >
-          <tr>
-            <th scope="col" class="px-2 md:px-6 py-3">
-              {{ $t("insidersTrades.table.insider") }}
-            </th>
-            <th scope="col" class="px-2 md:px-6 py-3">
-              {{ $t("insidersTrades.table.date") }}
-            </th>
-            <th scope="col" class="px-2 md:px-6 py-3">
-              {{ $t("insidersTrades.table.transaction") }}
-            </th>
-            <th scope="col" class="px-2 md:px-6 py-3">
-              {{ $t("insidersTrades.table.value") }}
-            </th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr
-            v-for="(trade, i) of insidersTrades"
-            :key="i"
-            class="border-b dark:border-gray-700"
-            :class="{
-              'bg-red-100': trade.transaction === 'Sale',
-              'bg-green-100': trade.transaction === 'Buy',
-            }"
+          <thead
+            class="text-xs text-gray-700 uppercase border-b-4 dark:text-gray-400"
           >
-            <th
-              scope="row"
-              class="px-2 md:px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+            <tr>
+              <th scope="col" class="px-2 md:px-6 py-3">
+                {{ $t("insidersTrades.table.insider") }}
+              </th>
+              <th scope="col" class="px-2 md:px-6 py-3">
+                {{ $t("insidersTrades.table.date") }}
+              </th>
+              <th scope="col" class="px-2 md:px-6 py-3">
+                {{ $t("insidersTrades.table.transaction") }}
+              </th>
+              <th scope="col" class="px-2 md:px-6 py-3">
+                {{ $t("insidersTrades.table.value") }}
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr
+              v-for="(trade, i) of insidersTrades"
+              :key="i"
+              class="border-b dark:border-gray-700"
+              :class="{
+                'bg-red-100': trade.transaction === 'Sale',
+                'bg-green-100': trade.transaction === 'Buy',
+              }"
             >
-              {{ trade.insiderTrading }}
-            </th>
-            <td class="px-2 md:px-6 py-4">{{ trade.date }}</td>
-            <td class="px-2 md:px-6 py-4">{{ trade.transaction }}</td>
-            <td class="px-2 md:px-6 py-4">{{ trade.value }}</td>
-          </tr>
-        </tbody>
-      </table>
+              <th
+                scope="row"
+                class="px-2 md:px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+              >
+                {{ trade.insiderTrading }}
+              </th>
+              <td class="px-2 md:px-6 py-4">{{ trade.date }}</td>
+              <td class="px-2 md:px-6 py-4">{{ trade.transaction }}</td>
+              <td class="px-2 md:px-6 py-4">{{ trade.value }}</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
     </div>
+    <HiddenParagraph>
+      {{ $t("insidersTrades.disclaimer") }}
+    </HiddenParagraph>
   </div>
 </template>
