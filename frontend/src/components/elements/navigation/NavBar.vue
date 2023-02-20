68300cc38f30f1f0bacf124bf08c0b9b46977039
samgozman <sam@gozman.space>
2023-02-20 09:16:35 +0400

    Removed the yet useless link

--- frontend/src/components/elements/navigation/NavBar.vue
+++ frontend/src/components/elements/navigation/NavBar.vue
@@ -89,13 +89,6 @@ import LanguageSwitcher from "@/components/elements/LanguageSwitcher.vue";
             <a
               href="#"
               class="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
-              >{{ $t("nav.projects") }}</a
-            >
-          </li>
-          <li>
-            <a
-              href="#"
-              class="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
               >{{ $t("nav.support") }}</a
             >
           </li>
