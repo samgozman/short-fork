a87d4213786b1e4907dc60b01ac9f73e96ddae84
samgozman <sam@gozman.space>
2023-02-20 09:18:23 +0400

    Add source code link

--- frontend/src/components/elements/navigation/NavBar.vue
+++ frontend/src/components/elements/navigation/NavBar.vue
@@ -94,7 +94,9 @@ import LanguageSwitcher from "@/components/elements/LanguageSwitcher.vue";
           </li>
           <li>
             <a
-              href="#"
+              href="https://github.com/samgozman/short-fork"
+              target="_blank"
+              rel="nofollow noopener noreferrer"
               class="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
               >{{ $t("nav.code") }}</a
             >
