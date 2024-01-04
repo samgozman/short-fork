84be784c74ee9dfc34b0091457f7d2ade3a90a7f
samgozman <sam@gozman.space>
2024-01-04 11:31:03 +0200

    disable attention box

--- frontend/src/components/elements/navigation/NavBar.vue
+++ frontend/src/components/elements/navigation/NavBar.vue
@@ -7,7 +7,7 @@ import ThemeSwitcher from "@/components/elements/themeSwitcher/ThemeSwitcher.vue
   <nav
     class="bg-white border-gray-200 px-2 sm:px-4 py-2 rounded-b dark:bg-slate-700 shadow-sm"
   >
-    <div class="container flex flex-wrap items-center justify-between mx-auto">
+    <!-- <div class="container flex flex-wrap items-center justify-between mx-auto">
       <p class="w-full text-center dark:text-white font-medium">
         <span class="font-bold">{{ $t("nav.attention.title") }}</span>
         {{ $t("nav.attention.messageOne") }}
@@ -17,7 +17,7 @@ import ThemeSwitcher from "@/components/elements/themeSwitcher/ThemeSwitcher.vue
           >https://short-fork.extr.app</a
         >. {{ $t("nav.attention.messageTwo") }}
       </p>
-    </div>
+    </div> -->
     <div class="container flex flex-wrap items-center justify-between mx-auto">
       <a href="#" class="flex items-center md:w-[160px]">
         <span
